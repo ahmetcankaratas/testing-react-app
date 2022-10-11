c139c8e9e3e39b40a7299ce35f81165946210c62
lingardium <karatascanahmet@yahoo.com>
2022-10-11 08:27:58 +0000

    add first test

---
+++ src/components/Greeting.js
@@ -0,0 +1,15 @@
+const Greeting = () => {
+  return (
+    <div>
+      <h2>Hello World!</h2>
+      <p>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+        explicabo.
+      </p>
+    </div>
+  );
+};
+
+export default Greeting;
---
+++ src/components/Greeting.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import Greeting from "./Greeting";
+
+test("renders Hello World as a text", () => {
+  // # 1 - Arrange
+  render(<Greeting />);
+
+  // # 2 - Act
+  // ... nothing
+
+  // Assert
+  const element = screen.getByText('Hello World!', {exact: true});
+  expect(element).toBeInTheDocument();
+});
