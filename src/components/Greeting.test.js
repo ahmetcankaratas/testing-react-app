a0dbc2c616c4dd398c8b155b0814d3bb9d834360
lingardium <karatascanahmet@yahoo.com>
2022-10-11 08:32:10 +0000

    add testing suite

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,14 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
 
-test("renders Hello World as a text", () => {
-  // # 1 - Arrange
-  render(<Greeting />);
+describe("Greeting component", () => {
+  test("renders Hello World as a text", () => {
+    // # 1 - Arrange
+    render(<Greeting />);
 
-  // # 2 - Act
-  // ... nothing
+    // # 2 - Act
+    // ... nothing
 
-  // Assert
-  const element = screen.getByText('Hello World!', {exact: true});
-  expect(element).toBeInTheDocument();
+    // Assert
+    const element = screen.getByText("Hello World!", { exact: true });
+    expect(element).toBeInTheDocument();
+  });
 });
